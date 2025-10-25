8922dab54a035d44e58a2c97d4b536e7d0e60e28
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-10-25 20:13:30 +0530

    Commit for Day 1

---
+++ first-java/src/HelloNewJava.java
@@ -0,0 +1,5 @@
+public class HelloNewJava {
+    public static void main(String[] args) {
+        System.out.println("Hello New Java.");
+    }
+}
---
+++ first-java/src/Variables.java
@@ -0,0 +1,46 @@
+public class Variables {
+
+    /**
+     * doc comments
+     */
+
+    public static void main(String[] args) {
+        // Variables
+
+        // Declaration
+        int a;
+        String name;
+
+        // Initialization
+        a = 10;
+        name = "Java";
+
+        int age = 30;
+
+//        System.out.println(a);
+//        System.out.println(name);
+
+//        System.out.println(STR."Hello, \{name}. You are \{age} years old."); // Version 21, upper not supported
+//        System.out.println("Hello, " + name + ". You are " + age + " years old.");
+
+        String myName = "Sankar";
+        int myAge = 20;
+        String favLanguage = "Java";
+
+        System.out.println("Hello, my name is " + myName + ". I am " + age + " years old and my favourite programming language is " + favLanguage);
+
+        // Constants
+        final int CONSTANT_VALUE = 3;
+//        CONSTANT_VALUE = 5; // Not changeable
+
+
+        // Single Line Comment
+
+        /*
+        Multi
+        line
+        comments
+         */
+
+    }
+}
